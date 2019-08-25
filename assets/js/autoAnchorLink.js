3eb8e4547d45adbee36556b207d8d4519d59a38f
Amir Shahmoradi <a.shahmoradi@gmail.com>
2019-08-25 18:34:51 -0500

    latest build

--- assets/js/autoAnchorLink.js
+++ assets/js/autoAnchorLink.js
@@ -1,102 +1,87 @@
 // Generate for each header, a different anchor link symbol
 // $(function() {
 //   return $("h2, h3, h4, h5, h6").each(function(i, el) {
-//     var $el, suffix, prefix, id;
+//     var $el, icon, id;
 //     $el = $(el);
 //     id = $el.attr('id');
-//     suffix = '<i class="fa fa-link"></i>';
+//     icon = '<i class="fa fa-anchor"></i>';
 //     if (id) {
-//       return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+//       return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
 //     }
 //   });
 // });
 
 $(function() {
   return $("h2").each(function(i, el) {
-    var $el, suffix, prefix, id;
+    var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     if (id) {
-        suffix = '<i class="fa fa-link"></i>';
-        prefix = '<i class="fa fa-link fa-flip-horizontal"></i>';
-        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(prefix));
-        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+        icon = '<i class="fa fa-anchor"></i>';
+        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
   });
 });
 
 $(function() {
   return $("h3").each(function(i, el) {
-    var $el, suffix, prefix, id;
+    var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     if (id) {
-        suffix =   '<i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>';
-        prefix =   '<i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>';
-        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(prefix));
-        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+        icon =   '<i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>';
+        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
   });
 });
 
 $(function() {
   return $("h4").each(function(i, el) {
-    var $el, suffix, prefix, id;
+    var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     if (id) {
-        suffix =   '<i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>';
-        prefix =   '<i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>';
-        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(prefix));
-        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+        icon =   '<i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>';
+        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
   });
 });
 
 $(function() {
   return $("h5").each(function(i, el) {
-    var $el, suffix, prefix, id;
+    var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     if (id) {
-        suffix =   '<i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>';
-        prefix =   '<i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>';
-        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(prefix));
-        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+        icon =   '<i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>';
+        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
   });
 });
 
 $(function() {
   return $("h6").each(function(i, el) {
-    var $el, suffix, prefix, id;
+    var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     if (id) {
-        suffix =   '<i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>\
-                    <i class="fa fa-link"></i>';
-        prefix =   '<i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>\
-                    <i class="fa fa-link fa-flip-horizontal"></i>';
-        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(prefix));
-        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(suffix));
+        icon =   '<i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>\
+                    <i class="fa fa-anchor"></i>';
+        $el = $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+        return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
   });
 });
