6d80b0bdeb394b9a65be92f42314ba305566b042
Amir Shahmoradi <a.shahmoradi@gmail.com>
2019-10-12 01:20:48 -0500

    latest build

--- programming/fibonacci-sequence-via-recursive-function-calls/fib.m
+++ programming/fibonacci-sequence-via-recursive-function-calls/fib.m
@@ -15,14 +15,14 @@ function fib()
         disp('The input argument is not a non-negative integer!');
         fib()
     end
-    
+
     function fib = getFib(n_int)
-        if n_int == 0
-            fib = 0;
+        if n_int > 1
+            fib = getFib(n_int-1) + getFib(n_int-2);
         elseif n_int == 1
             fib = 1;
         else
-            fib = getFib(n_int-1) + getFib(n_int-2);
+            fib = 0;
         end
     end
 
