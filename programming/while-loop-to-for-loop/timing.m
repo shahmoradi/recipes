d549253cbbc8aff0096fe66d39006d585a889e1d
Amir Shahmoradi <a.shahmoradi@gmail.com>
2024-08-03 21:52:00 -0400

    latest build

--- programming/while-loop-to-for-loop/timing.m
+++
@@ -1,4 +0,0 @@
-InVec = randi(100,1,10^7);
-disp(['Timing for convertTempVec: '  , num2str(timeit(@()convertTempVec(InVec,'C2F'))), ' seconds.'   ]);
-disp(['Timing for convertTempFor: '  , num2str(timeit(@()convertTempVec(InVec,'C2F'))), ' seconds.'   ]);
-disp(['Timing for convertTempWhile: ', num2str(timeit(@()convertTempWhile(InVec,'C2F'))), ' seconds.' ]);
